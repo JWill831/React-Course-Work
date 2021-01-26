3967ea93f0ca652adf02d20b014f71bcb4fae5dc
JWill831 <jon.fogtown@gmail.com>
2021-01-26 11:51:11 -0800

    166 fetching data with no loop

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,25 +8,43 @@ import './Blog.css';
 import axios from 'axios';
 
 class Blog extends Component {
-
+    state={
+        posts:[],
+        selectedPostId: null
+    }
 
     componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(res=>{
-        
-                console.log("Response", res);
+                const posts=res.data.slice(0, 4);
+                const updatedPosts=posts.map(post=> {
+                    return {
+                        ...post, 
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts:updatedPosts})
+                // console.log("Response", res);
             });
     }
+    postSelectedHandler=(id)=>{
+    this.setState({selectedPostId: id})
+    }
     render () {
+        const posts=this.state.posts.map(post=>{
+            return <Post
+            clicked={()=> this.postSelectedHandler(post.id)}
+            key={post.id} 
+            title={post.title} 
+            author={post.author}/>
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
