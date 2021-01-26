164f6d14f082f159f6a021da485f666a8eb5fd79
JWill831 <jon.fogtown@gmail.com>
2021-01-25 16:55:44 -0800

    adding componentDidMount()

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
+
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res=>{
+        
+                console.log("Response", res);
+            });
+    }
     render () {
         return (
             <div>
